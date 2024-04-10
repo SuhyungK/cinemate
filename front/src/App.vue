ee728310a9de3b5d3ff490d14e8b34865547a0a1
suhyungk <dook4966@gmail.com>
2024-04-10 17:05:25 +0900

    init : 개발 환경 세팅
    back/front 폴더 분리 및 spring, vue 프레임워크 환경 설정

---
+++ front/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { onMounted } from 'vue'
+
+  onMounted(() => {
+    const API_URL = "http://localhost:8080/api/v1"
+    fetch(API_URL)
+      .then(res => console.log("안녕"))
+  })
+</script>
+
+<template>
+  <button>
+    버튼 클릭
+  </button>
+</template>
