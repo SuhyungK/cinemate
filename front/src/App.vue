bbbf2728609924b3fdf1cad7fb853e1bc5050568
suhyungk <dook4966@gmail.com>
2024-04-11 22:42:02 +0900

    front - 전체 영화 조회 구현
    메인 화면 접속 시 전체 영화 조회 API 호출 후 제목과 포스터 이미지 보여주는 화면 구현

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,24 @@
 <script setup>
-  import { onMounted } from 'vue'
+import { onMounted, ref } from "vue";
 
-  onMounted(() => {
-    const API_URL = "http://localhost:8080/api/v1"
-    fetch(API_URL)
-      .then(res => console.log("안녕"))
-  })
+let count = ref(0);
+let title = "Hello Vue";
+var movieList = ref([]);
+const imgUrl = "https://image.tmdb.org/t/p/w500";
+
+onMounted(() => {
+  const API_URL = "http://localhost:8080/api/v1/movies";
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      movieList.value.push(data[0]);
+      movieList.value.push(data[1]);
+      movieList.value.push(data[2]);
+      console.log(data);
+    });
+});
 </script>
 
-<template>
-  <button>
-    버튼 클릭
-  </button>
-</template>
+<template></template>
+
+<style scoped></style>
